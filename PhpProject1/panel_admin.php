7d23f897d21eb45fdd20db1cffdff986b679f08a
Marcury <marcinpiechnik9@wp.pl>
2017-05-10 16:03:04 +0200

    Page with log in to administrator panel where you can edit guestbook entries.

---
+++ PhpProject1/panel_admin.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>HEAVEN</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/layout.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="css/prettyPhoto.css" type="text/css">
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/sprites.js"></script>
+<script type="text/javascript" src="js/gSlider.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+
+</head>
+<body id="page7">
+<div id="main">
+	<header>
+		<nav>
+			<ul>
+			<li><a href="index.html">O nas</a></li>
+			<li><a href="index-1.html">Audio</a></li>
+			<li><a href="index-2.html">Video</a></li>
+			<li><a href="index-3.html">Galeria</a></li>
+			<li><a href="index-4.html">Kontakt</a></li>
+			<li><a href="index-5.php">Goście</a></li>
+                        <li class="active"><a href="panel_admin.php">Admin</a></li>
+                        </ul>
+	</nav>
+
+	
+</header>
+  <article id="content">
+	<div class="col-1">
+		<div class="p2">
+		<h2>Nowe Zdjęcia</h2>
+		<img src="images/col-1-img.jpg" class="p1" alt=""> <a href="index-3.html" class="more">Więcej</a></div>
+		<div class="p2">
+		<h2>Nowy Film</h2>
+                <a href="video/video_AS3.swf?width=512&amp;height=288&amp;fileVideo=temp_video.flv" rel="prettyPhoto"><img class="p1" src="images/col1-video-thumb1.jpg" alt=""></a>
+		<div class="alc"><a href="index-2.html">Więcej Filmów</a></div>
+		</div>
+	</div>
+	<div class="col-2">
+		<!-- audio player begin -->
+		
+		<!-- audio player end -->
+		
+		<div class="table-border">
+                    <form action="php/checkuser.php" method="POST" id="login-form">
+                        <fieldset>
+                            <dl>
+                                <dt><label for="nick">Login:</label></dt>
+                                <dd><input type="text" name="login" id="nick" placeholder="Login" pattern="[a-zA-Z0-9]{5,20}" required title="Od 5 do 20 znaków. Polskie znaki nie są dozwolone."></dd>
+                                <dt><label for="password">Hasło:</label></dt>
+                                <dd><input type="password" name="password" id="password" placeholder="Hasło" pattern="[a-zA-Z0-9]{5,18}" required title="Od 5 do 20 znaków. Polskie znaki nie są dozwolone."></dd>
+                                <dt><input type="submit" name="check" value="Zaloguj"></dt>
+                            </dl>
+                        </fieldset>
+                        
+                    </form>
+                    <p><a href="php/form.php">Rejestracja</a></p>
+		</div>
+	</div>
+	<div class="col-3">
+		<h2>Ostatnie Wpisy</h2>
+		<div class="und">
+		<p>Coś tam coś tam, do uzupełnienia<br>
+			<a href="index-5.php">1h temu</a></p>
+		<p>Coś tam coś tam, do uzupełnienia<br>
+			<a href="index-5.php">23h temu</a></p>
+		<p>Coś tam coś tam, do uzupełnienia<br>
+			<a href="index-5.php">2d 7h temu</a></p>
+		<p>Coś tam coś tam, do uzupełnienia<br>
+			<a href="index-5.php">15d 7h temu</a></p>
+		<p>Coś tam coś tam, do uzupełnienia<br>
+			<a href="index-5.php">25d 7h temu</a></p>
+		</div>
+		
+		<h2>ZNAJDŹ NAS!</h2>
+		<ul class="soc-ico">
+		<li><a href="https://www.facebook.com/grupaheaven?fref=ts"><img src="images/facebook.png" alt=""></a></li>
+		<li><a href="#"><img src="images/twitter.png" alt=""></a></li>
+		<li><a href="#"><img src="images/myspace.png" alt=""></a></li>
+		<li><a href="#"><img src="images/linkedin.png" alt=""></a></li>
+		</ul>
+	</div>
+  </article>
+  <div class="af clear"></div>
+</div>
+<footer>
+	<span>
+            <a>GRUPA MUZYCZNA HEAVEN. grupaheaven@go2.pl tel. 608 330 811</a><br>
+            <a>Dumnie wspierane przez WordPressa</a>
+	</span>
+</footer>
+<script type="text/javascript">Cufon.now();
+$(function(){
+	$('nav,.more,.header-more').sprites();
+
+	$('.header-slider').gSlider({
+		prevBu:'.hs-prev',
+		nextBu:'.hs-next'
+	});
+});
+$(window).load(function(){
+	$('.tumbvr')._fw({tumbvr:{
+		duration:2000,
+		easing:'easeOutQuart'
+	}})
+	.bind('click',function(){
+		location="index-3.html";
+	});
+	
+	$('a[rel=prettyPhoto]').each(function(){
+		var th=$(this),
+			pb;
+		th
+			.append(pb=$('<span class="playbutt"></span>').css({opacity:.7}));
+		pb
+			.bind('mouseenter',function(){
+				$(this)
+					.stop()
+					.animate({opacity:.9});
+			})
+			.bind('mouseleave',function(){
+				$(this)
+					.stop()
+					.animate({opacity:.7});
+			});
+	})
+	.prettyPhoto({theme:'dark_square'});
+});
+</script>
+</body>
+</html>
